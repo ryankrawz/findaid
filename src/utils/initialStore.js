3a2d05210a78b5759519e25d640be0dc8d7e7ab3
ryankrawz <krawczry@bc.edu>
2021-04-17 13:33:52 -0400

    Sample store

---
+++ src/utils/initialStore.js
@@ -0,0 +1,49 @@
+const initialStore = {
+    users: [
+        {
+            email: 'bernie.sanders@gmail.com',
+            password: 'vermontmaplesyrup',
+            name: 'Bernie Sanders',
+            bio: 'I am once again asking for your financial support.',
+            photo: 'berniesanders.jpg',
+            isSuperUser: true,
+            saved: ['357458']
+        }
+    ],
+    posts: [
+        {
+            id: '357458',
+            title: 'Landfill in Underprivileged Area',
+            location: 'Atlanta, GA',
+            description: 'A new landfill site has been marked in an impoverished neighborhood without the community members\' consent.',
+            images: ['landfill.jpg', 'atlantaneighborhood.jpg'],
+            resourcesInfo: [
+                {
+                    name: 'Landfill Zoning Laws',
+                    link: 'landfillagency.org'
+                }
+            ],
+            fundsPetitions: [
+                {
+                    name: 'Stop the Landfill Fund',
+                    link: 'stopthelandfill.com'
+                }
+            ],
+            events: [
+                {
+                    name: 'Town Hall Meeting',
+                    link: 'atlanta.org'
+                }
+            ],
+            comments: [
+                {
+                    user: 'bernie.sanders@gmail.com',
+                    text: 'This outrageous! We must rally for change.'
+                }
+            ]
+        }
+    ],
+
+}
+
+export default initialStore;
