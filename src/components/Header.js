7fe9c5d831cf4b39d6b2bbfabce851d8543f5056
Joy <joyh.son@gmail.com>
2021-04-23 06:31:33 -0400

    added header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import css from './Header.module.css';
+import publicUrl from 'utils/publicUrl';
+import {
+    Link
+  } from "react-router-dom";
 
-function Header() {
+function Header(props) {
     return (
-        <div>
-            Header
+        <div className={css.header}>
+            <div className={css.headerItem}>
+                <Link to="/">
+                    <img src='assets/home.svg' alt="Home"/>
+                </Link>
+            </div>
+            <div className={css.headerItem}>
+                FindAid
+            </div>
+            <div className={css.headerItem}>
+                <Link to="profile">
+                    <img src='assets/profile.svg' alt="Profile"/>
+                </Link>
+            </div>
+
         </div>
     )
 }
